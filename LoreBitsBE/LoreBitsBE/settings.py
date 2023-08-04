75848092559fbab7153a093b3712b430f177a53a
KrzysiuKulakTooploox <krzysztof.kulak@tooploox.com>
2023-08-04 18:27:00 +0200

    add djangorestframework, add generic views for listing and creating Settings, creating Lores and destroying both of the models

--- LoreBitsBE/LoreBitsBE/settings.py
+++ LoreBitsBE/LoreBitsBE/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "lorebits.apps.LorebitsConfig",
     "polls.apps.PollsConfig",
+    "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
